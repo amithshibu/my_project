4019266b9685cf4f3cce9896b916aea13ffa945e
amithshibu <amithshibu2002@gmail.com>
2024-02-24 00:00:03 +0530

    Initial commit

---
+++ tt.java
@@ -0,0 +1,14 @@
+public class qq {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 3;
+        //int num2 = 4;
+        int num2 = 5;
+        int x=888;
+        // Calculate the sum
+        int sum = num1 + num2+x;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
